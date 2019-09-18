1b5fe8c4761ce422223b3e8dac58dc1e9f4dcae5
19214KseniaLyashchenko <55023695+19214KseniaLyashcsenko@users.noreply.github.com>
2019-09-18 22:26:08 +1100

    Update Homework1.hs

--- Homework1.hs
+++ Homework1.hs
@@ -1,6 +1,6 @@
 result a b c | (d > 0) = (x1,x2)
              | (d == 0) = (x,x)
-			 | (d < 0 || ((b == 0) && (c>0)))= error "No roots"
+	     | (d < 0 || ((b == 0) && (c>0)))= error "No roots"
 	where
       d = b^2 - 4*a*c
       x1 = (-b + sqrt d)/(2*a)
