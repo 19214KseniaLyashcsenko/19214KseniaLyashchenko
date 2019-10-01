71db04b924d6b46b9883c63c9175f8a925aa4eec
19214KseniaLyashchenko <55023695+19214KseniaLyashcsenko@users.noreply.github.com>
2019-10-02 02:04:48 +1100

    Update Homework1.hs

--- Homework1.hs
+++ Homework1.hs
@@ -1,12 +1,12 @@
-result a b c | (d > 0) = (x1,x2)
-             | (d == 0) = (x,x)
-	     | (d < 0 || ((b == 0) && (c>0)))= error "No roots"
+result a b c  | (d >= 0 && a /= 0) = (x1,x2)
+              | (a == 0 && b /= 0) = (x,x)
+              | (a == 0 && b == 0 && c == 0) = error "Any x"
+              | ((a /= 0 && d < 0) || (a == 0 && b == 0 && c /= 0)) = error " No x "
 	where
       d = b^2 - 4*a*c
       x1 = (-b + sqrt d)/(2*a)
       x2 = (-b - sqrt d)/(2*a)
-      x = -b/(2*a)	  
-	  
+      x = -c/b
 	  
 	  
     
