d69d6c4e01c613cf7b8d350eda6fcaeb0dfb8a66
19214KseniaLyashchenko <55023695+19214KseniaLyashcsenko@users.noreply.github.com>
2019-11-12 20:08:57 +0700

    Update Homework1.hs

--- Homework1.hs
+++ Homework1.hs
@@ -1,12 +1,12 @@
+roots 0 _ _ = - (b / c)
+roots 0 0 _ = error "Корней нет"
 roots 0 0 0 = error "Any x"
 roots a b c   | (d >= 0 && a /= 0) = (x1,x2)
-              | (a == 0 && b /= 0) = (x,x)
               | ((a /= 0 && d < 0) || (a == 0 && b == 0 && c /= 0)) = error " No x "
       where
       d = b^2 - 4*a*c
       x1 = (-b + sqrt d)/(2*a)
       x2 = (-b - sqrt d)/(2*a)
-      x = -c/b
 	  
 	  
     
